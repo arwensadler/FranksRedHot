12757c53b58df5036e1dcff8119effe403e04beb
Aditya <asudhakar@olin.edu>
2019-04-08 19:11:46 -0400

    Added view.py file for the main pygame window

---
+++ view.py
@@ -0,0 +1,25 @@
+'''
+Component Class for Frank's Red Hot SPICE Software
+Author(s): @awenstrup, @aditya-sudhakar, @MellieZito, @arwensadler
+
+Function: Main Window for Pick-and-Place
+'''
+
+import pygame
+
+
+background_color = (255,255,255)
+(width, height) = (300, 200)
+
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Test Window')
+screen.fill(background_color)
+
+
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
